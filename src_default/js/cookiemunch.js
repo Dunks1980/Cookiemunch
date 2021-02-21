3ccc68aed593a7812290d2463d4eee434ad26d13
Ian Dunkerley <dunks1980@gmail.com>
2021-02-21 17:13:01 +0000

    made callback iffe

--- src_default/js/cookiemunch.js
+++ src_default/js/cookiemunch.js
@@ -846,9 +846,9 @@ const cookiemunch_function = (options_passed, block_functions, callback) => {
       load_dropdown();
     }
     if (typeof block_functions === "function") {
-      block_functions();
+      (() => {block_functions();})();
     } else if (typeof callback === "function") {
-      callback();
+      (() => {callback();})();
     }
   };
 
